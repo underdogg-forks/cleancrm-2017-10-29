0868dc8c79f8407e4ab583751cf3133c3b1b440e
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-11-05 21:56:20 +0800

    rename app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
      */
 
-    'name' => 'S.plate',
+    'name' => 'splate',
 
     'themes' => [
         'default' => [
